127ea39d732c772fe594cb9ec02dcf6653d19115
LUIS GERARDO AGUILAR GARCIA <100291529+LuisAguilarDev@users.noreply.github.com>
2025-02-15 11:08:30 -0500

    feat: sliding puzzle

--- sliding_puzzle/index.py
+++ sliding_puzzle/index.py
@@ -1,107 +1,130 @@
-puzzle = [[2,3],
-          [0,1]]
-puzzle2 = [[1,2],
-          [3,0]]
+import heapq
+import time
+def find_zero(matrix):
+    """Finds and returns the (i, j) position of the element 0 in the matrix."""
+    for r, row in enumerate(matrix):
+        for c, col in enumerate(row):
+            if col == 0:
+                return r, c
+    raise ValueError("Matrix does not contain a zero.")
 
-puzzle3 = [[3,0],
-           [2,1]]
-# solution is [[1,2],
-#              [3,0]]
+# no heuristic
+def find_solution(matrix,solution):
+    IR = len(matrix)
+    IC = len(matrix[0])
+    directions = [(1,0),(0,1),(-1,0),(0,-1)]
+    visited = [matrix]
+    queue = [(matrix,0)]
+    while queue:
+        cm,steps = queue.pop(0)
+        if(cm == solution):
+            return (cm,steps)
+        # Find the position of zero
+        r, c = find_zero(cm)
+        for dr,dc in directions:
+            nr, nc = r + dr, c + dc
+            if nr >= IR or nr < 0 or nc < 0 or nc >= IC: continue
+            # Convert to a mutable list of lists
+            mut = [list(row) for row in cm]
+            # Swap the element 0 with the element at the new position
+            mut[r][c], mut[nr][nc] = mut[nr][nc], mut[r][c]
+            next_m = tuple(tuple(row) for row in mut)
+            if(next_m not in visited):
+                visited.append(next_m)
+                queue.append((next_m,steps+1))
+    return (None,-1)
 
-def solve(puzzle):
-    if isValid(puzzle): return 0
-    IR = len(puzzle) - 1
-    IC = len(puzzle[0]) - 1
-    directions = [(-1,0),(0,-1),(1,0),(0,1)]
-    r,c  = find(0,puzzle)
-    can_move = []
-    for dr,dc in directions:
-        nr,nc = r + dr, c + dc
-        if nr < 0 or nc < 0 or nr > IR or nc > IC:continue
-        print(nr,nc)
-        value = puzzle[nr][nc]
-        if value:
-            can_move.append(value)
-    to_move = min(can_move)
-    to_r,to_c = find(to_move,puzzle)
- 
-    same_row = r == to_r
-    same_col = r == to_r
-    isClockwise = ((same_row and r == 1 and c - to_c > 0) or
-                    (same_row and r == 0 and c - to_c < 0) or 
-                    (same_col and c == 0 and r - to_r > 0) or
-                    (same_col and c == 1 and r - to_r < 0))
-    movements = 0
-    direction = [to_r - r,to_c - c]
-    while True:
-        dr,dc = direction
-        print(r,c,direction)
-        puzzle[r][c] = puzzle[r+dr][c+dc]
-        r,c = r + dr, c + dc
-        puzzle[r][c] = 0
-        #clockwise -1j counterclockwise 1j
-        n_direction = complex(*direction) * (-1j if isClockwise else 1j)
-        direction =  [int(n_direction.real), int(n_direction.imag)] 
-        movements += 1
-        if isValid(puzzle):
-            return movements
-        # if iteration == 5:
-        #     break
+# Example matrix as a tuple of tuples
+matrix = (
+    (5, 6, 4),
+    (3, 2, 7),
+    (8, 1, 0)
+)
 
-def isValid(puzzle):
-    num = 1
-    IR = len(puzzle)
-    IC = len(puzzle[0])
-    for r in range(0,IR):
-        for c in range(0,IC):
-            # print( (r == (IR - 1) and (IC - 1) == c))
-            # print(r,c,puzzle[r][c],(0 if (r != (IR - 1) and (IC - 1) != c) else num))
-            if puzzle[r][c] != (0 if (r == (IR - 1) and (IC - 1) == c) else num):
-                return False
-            num += 1
-    return True
+start_time = time.time()
+_,steps = find_solution(matrix,((1, 2, 3),(4, 5, 6), (7, 8, 0)))
+print("find_solution:",steps)
+end_time = time.time()
+execution_time = end_time - start_time
+print(f"Execution Time: {execution_time:.5f} seconds")
 
-def find(num,puzzle):
-    IR = len(puzzle)
-    IC = len(puzzle[0])
-    for r in range(0,IR):
-        for c in range(0,IC):
-            if puzzle[r][c] == num:
-                return (r,c)
-    return (-1,-1)
-# print(solve(puzzle))
-# print(solve(puzzle2))
-# print(solve(puzzle3))
+# Example matrix as a tuple of tuples
+matrix = (
+    (0, 1),
+    (3, 2),
+)
 
-#recorrer los grafos por ciudad
-#https://mega.nz/folder/fpVBTYDb#AhWVJ-CFyUA5mFHKIMZJjQ/folder/jpk1jAwR
+solution = (
+    (1, 2),
+    (3, 0),
+)
 
+puzzle = ((2,3),(0,1))
+puzzle2 = ((1,2),(3,0))
 
+puzzle3 = ((3,0),(2,1))
+unsolvable = ((0,2),(3,1))
+solution = ((1,2),(3,0))
+print(find_solution(puzzle,solution))
+print(find_solution(puzzle2,solution))
+print(find_solution(puzzle3,solution))
+print(find_solution(unsolvable,solution))
 
-def navigate_p2( grid, position ):
-    R, C = len(grid), len(grid[0])
+def heuristic(puzzle):
+    # check the movements to reach its place
+    goal_position = {
+        1: (0, 0), 2: (0, 1), 3: (0, 2),
+        4: (1, 0), 5: (1, 1), 6: (1, 2),
+        7: (2, 0), 8: (2, 1), 0: (2, 2)
+    }
+
+    distance = 0
+    for r in range(len(puzzle)):
+        for c in range(len(puzzle[0])):
+            c_value = puzzle[r][c]
+            if c_value != 0:  # Skip the empty tile
+                gr, gc = goal_position[c_value]
+                distance += abs(r - gr) + abs(c - gc)
     
-    visited = set()
-    direction = 0 - 1j
-    
-    while True:
-        r, c = int( position.imag ), int( position.real )
-        dr, dc = int( direction.imag), int( direction.real)
-        assert not( r >= R or r < 0 or c < 0 or c >= C )
-     
-        if (r,c,dr,dc) in visited:
-            #print( 'Cycle detected' )
-            return True
-        visited.add( (r,c,dr,dc) )
-            
-        nr, nc = r + dr, c + dc
-        if nr >= R or nr < 0 or nc < 0 or nc >= C:
-            #print( 'EXIT', (nr, nc) )
-            return False
-    
-        if grid[nr][nc] in '#O':
-            direction *= 1j
-        else:
-            position += direction
+    return distance
 
-print(0 - 1j)
+# with heutistic
+def find_solution_a_star(matrix,solution):
+    IR = len(matrix)
+    IC = len(matrix[0])
+    directions = [(1,0),(0,1),(-1,0),(0,-1)]
+    pq = []
+    visited_cost = {matrix:0}
+    heapq.heappush(pq,(heuristic(matrix),matrix,0))
+    while pq:
+        _,cm,steps = heapq.heappop(pq)
+        if(cm == solution):
+            return (cm,steps)
+        # Find the position of zero
+        r, c = find_zero(cm)
+        for dr,dc in directions:
+            nr, nc = r + dr, c + dc
+            if nr >= IR or nr < 0 or nc < 0 or nc >= IC: continue
+            # Convert to a mutable list of lists
+            mut = [list(row) for row in cm]
+            # Swap the element 0 with the element at the new position
+            mut[r][c], mut[nr][nc] = mut[nr][nc], mut[r][c]
+            next_m = tuple(tuple(row) for row in mut)
+            if (steps + 1) < visited_cost.get(next_m,float('inf')):
+                visited_cost[next_m] = steps + 1
+                priority = steps + 1 + heuristic(next_m)
+                heapq.heappush(pq,(priority,next_m,steps+1))
+    return (None,-1)
+
+matrix = (
+    (5, 6, 4),
+    (3, 2, 7),
+    (8, 1, 0)
+)
+
+start_time = time.time()
+_,steps = find_solution_a_star(matrix,((1, 2, 3),(4, 5, 6), (7, 8, 0)))
+print("find_solution_a_star:",steps)
+end_time = time.time()
+execution_time = end_time - start_time
+print(f"Execution Time: {execution_time:.5f} seconds")
